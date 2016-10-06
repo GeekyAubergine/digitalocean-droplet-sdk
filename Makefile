57c6235ebf970c9e600e226fa4f0dffaa592ee5a
Chris Aubert <c.aubergine@gmail.com>
2016-10-06 10:16:31 +0100

    Added linting. Closes #3

--- Makefile
+++ Makefile
@@ -2,3 +2,8 @@ test:
 	./node_modules/.bin/mocha --reporter spec
 
 .PHONY: test
+
+lint:
+	./node_modules/.bin/eslint ./**/*.js
+
+qa: lint test
--- test/test.js
+++ test/test.js
@@ -1,71 +1,72 @@
 var http = require('http'),
-    chai = require('chai'),    
-    should = chai.should(),
+    chai = require('chai'),
+    should = chai.should(), // eslint-disable-line no-unused-vars
     dropletSDK = require('../index'),
-    chaiAsPromised = require("chai-as-promised");
+    chaiAsPromised = require('chai-as-promised');
  
 chai.use(chaiAsPromised);
 
 var exampleDroplet = {
-	"droplet_id":2756294,
-	"hostname":"sample-droplet",
-	"vendor_data":"#cloud-config\ndisable_root: false\nmanage_etc_hosts: true\n\ncloud_config_modules:\n - ssh\n - set_hostname\n - [ update_etc_hosts, once-per-instance ]\n\ncloud_final_modules:\n - scripts-vendor\n - scripts-per-once\n - scripts-per-boot\n - scripts-per-instance\n - scripts-user\n",
-	"public_keys":["ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCcbi6cygCUmuNlB0KqzBpHXf7CFYb3VE4pDOf/RLJ8OFDjOM+fjF83a24QktSVIpQnHYpJJT2pQMBxD+ZmnhTbKv+OjwHSHwAfkBullAojgZKzz+oN35P4Ea4J78AvMrHw0zp5MknS+WKEDCA2c6iDRCq6/hZ13Mn64f6c372JK99X29lj/B4VQpKCQyG8PUSTFkb5DXTETGbzuiVft+vM6SF+0XZH9J6dQ7b4yD3sOder+M0Q7I7CJD4VpdVD/JFa2ycOS4A4dZhjKXzabLQXdkWHvYGgNPGA5lI73TcLUAueUYqdq3RrDRfaQ5Z0PEw0mDllCzhk5dQpkmmqNi0F sammy@digitalocean.com"],
-	"region":"nyc3",
-	"interfaces":{
-		"private":[
+	'droplet_id':2756294,
+	'hostname':'sample-droplet',
+	'vendor_data':'#cloud-config\ndisable_root: false\nmanage_etc_hosts: true\n\ncloud_config_modules:\n - ssh\n - set_hostname\n - [ update_etc_hosts, once-per-instance ]\n\ncloud_final_modules:\n - scripts-vendor\n - scripts-per-once\n - scripts-per-boot\n - scripts-per-instance\n - scripts-user\n',
+	'public_keys':['ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCcbi6cygCUmuNlB0KqzBpHXf7CFYb3VE4pDOf/RLJ8OFDjOM+fjF83a24QktSVIpQnHYpJJT2pQMBxD+ZmnhTbKv+OjwHSHwAfkBullAojgZKzz+oN35P4Ea4J78AvMrHw0zp5MknS+WKEDCA2c6iDRCq6/hZ13Mn64f6c372JK99X29lj/B4VQpKCQyG8PUSTFkb5DXTETGbzuiVft+vM6SF+0XZH9J6dQ7b4yD3sOder+M0Q7I7CJD4VpdVD/JFa2ycOS4A4dZhjKXzabLQXdkWHvYGgNPGA5lI73TcLUAueUYqdq3RrDRfaQ5Z0PEw0mDllCzhk5dQpkmmqNi0F sammy@digitalocean.com'],
+	'region':'nyc3',
+	'interfaces':{
+		'private':[
 			{
-				"ipv4":{
-					"ip_address":"10.132.255.113",
-					"netmask":"255.255.0.0",
-					"gateway":"10.132.0.1"
+				'ipv4':{
+					'ip_address':'10.132.255.113',
+					'netmask':'255.255.0.0',
+					'gateway':'10.132.0.1',
 				},
-				"mac":"04:01:2a:0f:2a:02",
-				"type":"private"
+				'mac':'04:01:2a:0f:2a:02',
+				'type':'private',
 			}, {
-				"ipv4":{
-					"ip_address":"10.132.255.114",
-					"netmask":"255.255.0.0",
-					"gateway":"10.132.0.1"
+				'ipv4':{
+					'ip_address':'10.132.255.114',
+					'netmask':'255.255.0.0',
+					'gateway':'10.132.0.1',
 				},
-				"ipv6":{
-					"ip_address":"2604:A880:0800:0010:0000:0000:017D:2001",
-					"cidr":64,
-					"gateway":"2604:A880:0800:0010:0000:0000:0000:0001"
+				'ipv6':{
+					'ip_address':'2604:A880:0800:0010:0000:0000:017D:2001',
+					'cidr':64,
+					'gateway':'2604:A880:0800:0010:0000:0000:0000:0001',
 				},
-				"mac":"04:01:2a:0f:2a:02",
-				"type":"private"
-			}
+				'mac':'04:01:2a:0f:2a:02',
+				'type':'private',
+			},
 		],
-		"public":[
+		'public':[
 			{
-				"ipv4":{
-					"ip_address":"104.131.20.105",
-					"netmask":"255.255.192.0",
-					"gateway":"104.131.0.1"
+				'ipv4':{
+					'ip_address':'104.131.20.105',
+					'netmask':'255.255.192.0',
+					'gateway':'104.131.0.1',
 				},
-				"ipv6":{
-					"ip_address":"2604:A880:0800:0010:0000:0000:017D:2001",
-					"cidr":64,
-					"gateway":"2604:A880:0800:0010:0000:0000:0000:0001"
+				'ipv6':{
+					'ip_address':'2604:A880:0800:0010:0000:0000:017D:2001',
+					'cidr':64,
+					'gateway':'2604:A880:0800:0010:0000:0000:0000:0001',
 				},
-				"mac":"04:01:2a:0f:2a:01",
-				"type":"public"}
-		]
+				'mac':'04:01:2a:0f:2a:01',
+				'type':'public',
+			},
+		],
 	},
-	"floating_ip": {
-		"ipv4": {
-			"active": true,
-			"ip_address": '127.0.0.1',
-		}
+	'floating_ip': {
+		'ipv4': {
+			'active': true,
+			'ip_address': '127.0.0.1',
+		},
 	},
-	"dns":{
-		"nameservers":[
-			"2001:4860:4860::8844",
-			"2001:4860:4860::8888",
-			"8.8.8.8"
-		]
-	}
+	'dns':{
+		'nameservers':[
+			'2001:4860:4860::8844',
+			'2001:4860:4860::8888',
+			'8.8.8.8',
+		],
+	},
 };
 
 var server = http.createServer(function(req, res) {       
@@ -122,7 +123,7 @@ describe('#droplet-api', function() {
 
 	it('gets private IP4 address', function() {
 		return dropletSDK.getPrivateIP4Addresses().should.eventually.deep.equal([exampleDroplet.interfaces.private[0].ipv4.ip_address,
-			exampleDroplet.interfaces.private[1].ipv4.ip_address])
+			exampleDroplet.interfaces.private[1].ipv4.ip_address]);
 	});
 
 	it('gets private IP6 address', function() {
@@ -138,7 +139,7 @@ describe('#droplet-api', function() {
 	});
 
 	it('gets public IP6 address', function() {
-		return dropletSDK.getPublicIP6Addresses().should.eventually.deep.equal([exampleDroplet.interfaces.public[0].ipv6.ip_address])
+		return dropletSDK.getPublicIP6Addresses().should.eventually.deep.equal([exampleDroplet.interfaces.public[0].ipv6.ip_address]);
 	});
 
 	it('determines if has floating ip', function() {
